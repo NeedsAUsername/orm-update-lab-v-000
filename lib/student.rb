20de2a4c0b36b01e2efeff58f75ad37e2e1e8b62
Kevin Chi <kevinchi118@gmail.com>
2018-07-17 22:13:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
 
-    attr_accessor :name, :grade, :id 
+    attr_accessor :name, :grade, :id
 
     def initialize(name, grade, id = nil)
         @name = name
@@ -42,13 +42,8 @@ class Student
 
     def self.create(name, grade)
         new = self.new(name, grade)
-        sql = <<-SQL
-            INSERT INTO students (name, grade)
-            VALUES (?, ?)
-        SQL
-
-        DB[:conn].execute(sql, new.name, new.grade)
-        new.id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+        new.save
+        new
     end
 
     def update
