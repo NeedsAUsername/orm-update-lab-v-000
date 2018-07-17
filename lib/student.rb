ad1c9b25c39d18f715edaa5967ff705768501b5c
Kevin Chi <kevinchi118@gmail.com>
2018-07-17 22:18:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,9 +56,9 @@ class Student
             WHERE students.name = ?
         SQL
 
-        student = DB[:conn].execute(sql, name)[0]
+        student = DB[:conn].execute(sql, name)
         self.new(student[1], student[2], student[0])
-    end 
+    end
 
     def update
 
